26110b7a4c51a6bb7196c3443647f1ed6f682d9f
Somesh4543 <Somesh.4543@gmail.com>
2023-09-16 17:09:49 +0530

    Assingment5.1

--- Week-5/Assingment5.1/Assingment5.1.1/Assingment5.1.1.js
+++ Week-5/Assingment5.1/Assingment5.1.1/Assingment5.1.1.js
@@ -1,13 +1,12 @@
 function* symbolIterator(arr) {
-    for (const str of arr) {
-      yield Symbol(str);
-    }
+  for (const str of arr) {
+    yield Symbol(str);
   }
-  
-  const input = ['hello', 'world', 'test'];
-  const iterator = symbolIterator(input);
-  
-  for (const symbol of iterator) {
-    console.log(symbol);
-  }
-  
+}
+
+const input = ["hello", "world", "test"];
+const iterator = symbolIterator(input);
+
+for (const symbol of iterator) {
+  console.log(symbol);
+}
