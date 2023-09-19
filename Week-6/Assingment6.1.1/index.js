f677d772236dd50f9d6a5c3df921dcb59110351c
Somesh4543 <Somesh.4543@gmail.com>
2023-09-19 17:56:00 +0530

    Assingment6.1

---
+++ Week-6/Assingment6.1.1/index.js
@@ -0,0 +1,7 @@
+import { add, subtract } from "./calculator.js";
+
+const resultAddition = add(2, 3);
+console.log(resultAddition);
+
+const resultSubtraction = subtract(2, 3);
+console.log(resultSubtraction);
